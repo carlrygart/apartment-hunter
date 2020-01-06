c683592732d5ef73fcdb27fb3b20397cf3c7359d
Carl Rygart <carl@rygart.se>
2020-01-06 19:28:50 +0100

    Update interval
    (cherry picked from commit 1ad6ffdb050194e07f092c8b975d8987ff8ed1df)

--- deploy/schedule.tf
+++ deploy/schedule.tf
@@ -1,11 +1,11 @@
-resource "aws_cloudwatch_event_rule" "every_ten_minutes" {
-  name = "every-ten-minutes"
-  description = "Fires every ten minutes"
-  schedule_expression = "rate(10 minutes)"
+resource "aws_cloudwatch_event_rule" "interval_trigger" {
+  name = "interval_trigger"
+  description = "Fires an event every minute"
+  schedule_expression = "rate(1 minute)"
 }
 
 resource "aws_cloudwatch_event_target" "run_hunter_schedule" {
-  rule = aws_cloudwatch_event_rule.every_ten_minutes.name
+  rule = aws_cloudwatch_event_rule.interval_trigger.name
   arn = aws_lambda_function.apartment_hunter.arn
 }
 
@@ -14,5 +14,5 @@ resource "aws_lambda_permission" "allow_cloudwatch_to_call_apartment_hunter" {
   action = "lambda:InvokeFunction"
   function_name = aws_lambda_function.apartment_hunter.function_name
   principal = "events.amazonaws.com"
-  source_arn = aws_cloudwatch_event_rule.every_ten_minutes.arn
+  source_arn = aws_cloudwatch_event_rule.interval_trigger.arn
 }
